317552d44263980585d909711c558d541bf8c37c
unknown <olysiuk01@gmail.com>
2020-09-12 18:06:15 +0300

    header

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,20 @@
-$(function(){
+//burger menu
 
+function menuBurger() {
+  const burger = document.querySelector(".header__burger");
+  const list = document.querySelector(".header__list");
+  const items = document.querySelectorAll(".header__list-item");
+  burger.addEventListener("click", () => {
+    burger.classList.toggle("header__burger--active");
+    list.classList.toggle("header__list--active");
+  });
 
+  items.forEach((item) => {
+    item.addEventListener("click", () => {
+      burger.classList.toggle("header__burger--active");
+      list.classList.toggle("header__list--active");
+    });
+  });
+}
 
-  
-});
+menuBurger();
